54787e9fb4aaa4445b463e1c4c755ae0a858fa10
Yoann Ochietti <yoann.ochietti@gmail.com>
2024-03-15 15:24:03 -0400

    Dashing cooldown changed to coutn from start of dash. Used to avoid issues with starting dash on frame where last one ended.

--- Assets/Scripts/Controllers/Mover.cs
+++ Assets/Scripts/Controllers/Mover.cs
@@ -14,7 +14,7 @@ public class Mover : MonoBehaviour
     [SerializeField]
     private float dashDuration;
 
-    //starts at end of dash
+    //starts at beginning of dash
     [SerializeField]
     private float dashCooldownDuration;
 
@@ -27,6 +27,7 @@ public class Mover : MonoBehaviour
     //disabled by knockbacker
     private bool inControl;
 
+    private bool dashing;
     private Vector2  dashingDirection;
     private float dashTimeRemaining;
     private float dashCooldownRemaining;
@@ -38,31 +39,47 @@ public class Mover : MonoBehaviour
         this.inControl = true;
         this.dashingDirection = Vector2.zero;
         this.dashTimeRemaining = 0;
+        this.dashing = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (this.inControl)
+        if (!this.inControl)
+            return;
+
+        bool wasDashingAtPreviousFrame = this.dashing;
+        if (this.dashTimeRemaining > 0)
         {
-            if (dashTimeRemaining > 0 && this.dashCooldownRemaining <= 0)
-            {                
-                this.rb.velocity = this.dashingDirection * this.dashSpeed;
-                this.dashTimeRemaining -= Time.fixedDeltaTime;
-                if (dashTimeRemaining <= 0)
-                {
-                    this.animator.SetBool("dashing", false);
-                    this.dashCooldownRemaining = this.dashCooldownDuration;
-                }
+            this.rb.velocity = this.dashingDirection * this.dashSpeed;
+            if (!wasDashingAtPreviousFrame)
+            {
+                //first dash frame
+                this.dashing = true;
+                this.animator.SetBool("dashing", true);
+                this.dashCooldownRemaining = this.dashCooldownDuration;
             }
             else
             {
-                this.rb.velocity = this.moveDirection * this.moveSpeed;
+                this.dashTimeRemaining -= Time.fixedDeltaTime;
                 this.dashCooldownRemaining -= Time.fixedDeltaTime;
             }
         }
+        else
+        {
+            if (wasDashingAtPreviousFrame)
+            {
+                //first frame out of dash
+                this.dashing = false;
+                this.animator.SetBool("dashing", false);
+            }
+
+            this.rb.velocity = this.moveDirection * this.moveSpeed;
+            this.dashCooldownRemaining -= Time.fixedDeltaTime;
+        }
     }
 
+    //Note : doesn't set dashing bool since we want to track whether actual movement has started for timing purposes
     public void Dash(Vector2 direction)
     {
         if (direction.magnitude == 0 || this.dashCooldownRemaining > 0)
@@ -70,7 +87,6 @@ public class Mover : MonoBehaviour
 
         this.dashingDirection = direction;
         this.dashTimeRemaining = this.dashDuration;
-        this.animator.SetBool("dashing", true);
     }
 
     internal void SetMovementDirection(Vector2 direction)
